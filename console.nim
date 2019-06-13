68b3b4deae94aae5cfa332f0cfc4e4715dd8518f
Jeffrey Massung <massung@gmail.com>
2019-06-12 20:29:45 -0500

    Fix unicode rendering on windows.

--- console.nim
+++ console.nim
@@ -19,13 +19,18 @@ when defined(windows):
     {.stdcall, dynlib: "kernel32", importc: "GetConsoleMode".}
   proc setConsoleMode(hConsoleHandle: Handle, dwMode: DWORD): WINBOOL
     {.stdcall, dynlib: "kernel32", importc: "SetConsoleMode".}
+  proc setConsoleOutputCP(page: int): WINBOOL
+    {.stdcall, dynlib: "kernel32", importc: "SetConsoleOutputCP".}
 
   # enable virtual terminal processing
   if getConsoleMode(handle, addr(mode)) != 0:
     discard setConsoleMode(handle, mode or 4)
+  
+  # enable code page UTF-8
+  discard setConsoleOutputCP(65001)
 
 # handle all other platforms
 enableTrueColors()
 
 # reset the console colors and attributes on exit
-system.addQuitProc(resetAttributes)
+addQuitProc(resetAttributes)
